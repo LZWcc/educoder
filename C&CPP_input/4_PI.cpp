4de0d926d4c3d153cd12848a17c9bb5110c530c9
LZWcc <55631433+LZWcc@users.noreply.github.com>
2024-10-16 18:36:09 +0800

    Create 4_PI.cpp

---
+++ C&CPP_input/4_PI.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+// 包含流操作算子库
+#include <iomanip>
+using namespace std;
+
+// 定义常量PI，后面可以直接用PI代替后面的数值
+#define PI 3.14159265358979323846
+
+int main()
+{
+    int n;
+    cin >> n;
+    // 请在Begin-End之间添加你的代码，输入n，按不同的精度输出 PI。
+    /********** Begin *********/
+    cout << fixed;
+    cout << setprecision(n) << PI << endl;
+    cout << setprecision(n+1) << PI << endl;
+    cout << setprecision(n+2) << PI << endl;
+    cout << setprecision(n+3) << PI << endl;
+    cout << setprecision(n+4) << PI << endl;
+    /********** End **********/
+    return 0;
+}
