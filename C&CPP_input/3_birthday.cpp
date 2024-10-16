068b42f9ffb14061f505068de97cded3d242b658
LZWcc <55631433+LZWcc@users.noreply.github.com>
2024-10-16 18:34:41 +0800

    Create 3_birthday.cpp

---
+++ C&CPP_input/3_birthday.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int day,month,year;
+    cin >> day >> month >> year;
+    cout <<"Hello! " << day << " " << month << " " << year ;
+
+
+    return 0;
+}
