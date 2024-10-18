22101ec63e3f9263f02ae4725c00b6d67a178cc0
LZWcc <55631433+LZWcc@users.noreply.github.com>
2024-10-19 00:15:43 +0800

    Create 02_sum&average.c

---
+++ 01_essential_in&output_operate/02_sum&average.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;   
+    scanf("%d %d %d",&a,&b,&c);
+    int sum = a + b + c ;
+    printf("%d ",sum);
+
+    double aver=(double) sum/3;
+    printf("%.1f",aver);
+    
+    
+    return 0;
+
+}
