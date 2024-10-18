841fbc3748a32ce91640299b6bcea6504be01675
LZWcc <55631433+LZWcc@users.noreply.github.com>
2024-10-19 00:16:15 +0800

    Create 03_ball.c

---
+++ 01_essential_in&output_operate/03_ball.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#define PI 3.14159
+int main()
+{
+    double r;
+    scanf("%lf",&r);
+    double s =(double) 4*PI*r*r;
+    double v =(double) PI*r*r*r*(4.0/3.0);
+    printf("s = %.3f\n",s);
+    printf("v = %.3f",v);
+
+    return 0;
+}
