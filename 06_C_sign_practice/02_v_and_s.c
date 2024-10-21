2cbc3fa94e3fb97fa1a880f74b03aa3140dd7991
LZWcc <55631433+LZWcc@users.noreply.github.com>
2024-10-21 13:40:40 +0800

    Create 02_v_and_s.c

---
+++ 06_C_sign_practice/02_v_and_s.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+   double t,a,v0,v,s;
+   /*********begin*********/
+    scanf("%lf %lf %lf",&t,&a,&v0);
+    v =v0 + a*t;
+    s =v0*t + 0.5*a*t*t;
+    printf("当初始速度为%.6lf时\n",v0);
+    printf("%.6lfs时的速度为%.6lf\n",t,v);
+    printf("%.6lfs时的位移为%.6lf",t,s);
+
+  /*********end*********/
+  return 0;
+}
