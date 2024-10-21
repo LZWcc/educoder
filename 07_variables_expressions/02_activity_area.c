e412fd1d6460486b24da3d53daccdf58f3a308da
LZWcc <55631433+LZWcc@users.noreply.github.com>
2024-10-21 16:55:47 +0800

    Create 02_activity_area.c

---
+++ 07_variables_expressions/02_activity_area.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#define PI 3.14
+int main(){
+    // 输入代码段
+    float r;
+    float s;
+    scanf("%f", &r);
+    s = (float)PI*r*r;
+    printf("活动面积为：%g平方千米",s);
+    
+    return 0;
+}
